bba84f7f726bacab0dcb7e9eff13ebce5171ffc7
devilroogan <85479917+devilroogan@users.noreply.github.com>
2021-09-12 00:50:45 +0530

    Update videoplayer.py

--- player/videoplayer.py
+++ player/videoplayer.py
@@ -8,37 +8,69 @@ from pytgcalls.types.input_stream import AudioParameters
 from pytgcalls.types.input_stream import InputAudioStream
 from pytgcalls.types.input_stream import InputVideoStream
 from pytgcalls.types.input_stream import VideoParameters
+
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from config import API_ID, API_HASH, SESSION_NAME
-from helper.decorators import authorized_users_only
+from config import API_ID, API_HASH, SESSION_NAME, BOT_USERNAME
+from helpers.decorators import authorized_users_only
+from helpers.filters import command
+from youtube_dl import YoutubeDL
+from youtube_dl.utils import ExtractorError
+
+SIGINT: int = 2
 
 app = Client(SESSION_NAME, API_ID, API_HASH)
 call_py = PyTgCalls(app)
-FFMPEG_PROCESSES = {}
+FFMPEG_PROCESS = {}
+
 def raw_converter(dl, song, video):
-    subprocess.Popen(
-        ['ffmpeg', '-i', dl, '-f', 's16le', '-ac', '1', '-ar', '48000', song, '-y', '-f', 'rawvideo', '-r', '20', '-pix_fmt', 'yuv420p', '-vf', 'scale=1280:720', video, '-y'],
+    return subprocess.Popen(
+        ['ffmpeg', '-i', dl, '-f', 's16le', '-ac', '1', '-ar', '48000', song, '-y', '-f', 'rawvideo', '-r', '20', '-pix_fmt', 'yuv420p', '-vf', 'scale=854:480', video, '-y'],
         stdin=None,
         stdout=None,
         stderr=None,
         cwd=None,
     )
 
+def youtube(url: str):
+    try:
+        params = {"format": "best[height=?480]/best", "noplaylist": True}
+        yt = YoutubeDL(params)
+        info = yt.extract_info(url, download=False)
+        return info['url']
+    except ExtractorError: # do whatever
+        return 
+    except Exception:
+        return
 
-@Client.on_message(filters.command("stream"))
+
+@Client.on_message(command(["vplay", f"vplay@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
 @authorized_users_only
-async def stream(client, m: Message):
+async def startvideo(client, m: Message):
     replied = m.reply_to_message
     if not replied:
         if len(m.command) < 2:
-            await m.reply("`Reply to some Video File!`")
+            await m.reply("💡 ʀᴇᴘʟʏ ᴛᴏ ᴠɪᴅᴇᴏ ᴏʀ ᴘʀᴏᴠɪᴅᴇ ʏᴏᴜᴛᴜʙᴇ/ʟɪᴠᴇ ᴠɪᴅᴇᴏ ᴜʀʟ ᴛᴏ sᴛᴀʀᴛ ᴠɪᴅᴇᴏ sᴛʀᴇᴀᴍɪɴɢ")
         else:
             livelink = m.text.split(None, 1)[1]
             chat_id = m.chat.id
+            try:
+                livelink = await asyncio.wait_for(
+                    app.loop.run_in_executor(
+                        None,
+                        lambda : youtube(livelink)
+                    ),
+                    timeout=None # Add timeout (recommended)
+                )
+            except asyncio.TimeoutError:
+                await m.reply("TimeoutError: process is taking unexpected time")
+                return
+            if not livelink:
+                await m.reply("failed to get video data")
+                return
             process = raw_converter(livelink, f'audio{chat_id}.raw', f'video{chat_id}.raw')
-            FFMPEG_PROCESSES[chat_id] = process
-            msg = await m.reply("`Starting Video Stream...`")
+            FFMPEG_PROCESS[chat_id] = process
+            msg = await m.reply("🔁 **sᴛᴀʀᴛɪɴɢ ᴠɪᴅᴇᴏ sᴛʀᴇᴀᴍɪɴɢ...**")
             await asyncio.sleep(10)
             try:
                 audio_file = f'audio{chat_id}.raw'
@@ -57,23 +89,23 @@ async def stream(client, m: Message):
                     InputVideoStream(
                         video_file,
                         VideoParameters(
-                            width=1280,
-                            height=720,
+                            width=854,
+                            height=480,
                             frame_rate=20,
                         ),
                     ),
                     stream_type=StreamType().local_stream,
                 )
-                await msg.edit("**Started Video Stream!**")
+                await msg.edit("📽️ **ᴠɪᴅᴇᴏ sᴛʀᴇᴀᴍɪɴɢ sᴛᴀʀᴛᴇᴅ!**\n\n» ** ᴊᴏɪɴ ᴛᴏ ᴠɪᴅᴇᴏ ᴄʜᴀᴛ ᴏɴ ᴛʜᴇ ᴛᴏᴘ ᴛᴏ ᴡᴀᴛᴄʜ ᴛʜᴇ ᴠɪᴅᴇᴏ ʙʏ sᴘᴀʀᴋʏ✨🤍.**")
                 await idle()
             except Exception as e:
-                await msg.edit(f"**Error** -- `{e}`")
+                await msg.edit(f"🚫 **ᴇʀʀᴏʀ** | `{e}`")
    
     elif replied.video or replied.document:
-        msg = await m.reply("`Downloading...`")
+        msg = await m.reply("📥 ᴅᴏᴡɴʟᴏᴀᴅ ᴠɪᴅᴇᴏ...")
         video = await client.download_media(m.reply_to_message)
         chat_id = m.chat.id
-        await msg.edit("`Processing...`")
+        await msg.edit("🎦 **ᴘʀᴏᴄᴇssɪɴɢ...**")
         os.system(f"ffmpeg -i '{video}' -f s16le -ac 1 -ar 48000 'audio{chat_id}.raw' -y -f rawvideo -r 20 -pix_fmt yuv420p -vf scale=640:360 'video{chat_id}.raw' -y")
         try:
             audio_file = f'audio{chat_id}.raw'
@@ -99,19 +131,20 @@ async def stream(client, m: Message):
                 ),
                 stream_type=StreamType().local_stream,
             )
-            await msg.edit("**Started Video Stream!**")
+            await msg.edit("📽️ **ᴠɪᴅᴇᴏ sᴛʀᴇᴀᴍɪɴɢ sᴛᴀʀᴛᴇᴅ**\n\n» **ᴊᴏɪɴ ᴛᴏ ᴠɪᴅᴇᴏ ᴄʜᴀᴛ ᴏɴ ᴛʜᴇ ᴛᴏᴘ ᴛᴏ ᴡᴀᴛᴄʜ ᴛʜᴇ ᴠɪᴅᴇᴏ ʙʏ sᴘᴀʀᴋʏ✨🤍.**")
         except Exception as e:
-            await msg.edit(f"**Error** -- `{e}`")
+            await msg.edit(f"🚫 **ᴇʀʀᴏʀ** | `{e}`")
             await idle()
     else:
-        await m.reply("`Reply to some Video!`")
+        await m.reply("💭 please reply to video or video file to stream")
 
-@Client.on_message(filters.command("stopstream"))
+
+@Client.on_message(command(["vstop", f"vstop@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
 @authorized_users_only
 async def stopvideo(client, m: Message):
     chat_id = m.chat.id
     try:
-        process = FFMPEG_PROCESSES.get(chat_id)
+        process = FFMPEG_PROCESS.get(chat_id)
         if process:
             try:
                 process.send_signal(SIGINT)
@@ -119,7 +152,7 @@ async def stopvideo(client, m: Message):
             except Exception as e:
                 print(e)
         await call_py.leave_group_call(chat_id)
-        await m.reply("**⏹️ Stop Video Stream!**")
+        await m.reply("✅ **ᴅɪsᴄᴏᴜɴᴛᴇᴅ ғʀᴏᴍ ᴠᴄ !**")
     except Exception as e:
-        await m.reply(f"**🚫 Error** - `{e}`")
+        await m.reply(f"🚫 **ᴇʀʀᴏʀ** | `{e}`")
         
