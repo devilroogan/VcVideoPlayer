36a57ae39da3cd0395a405f7cf6758e9e9a5a8ed
devilroogan <85479917+devilroogan@users.noreply.github.com>
2021-09-12 00:59:02 +0530

    Update videoplayer.py

--- player/videoplayer.py
+++ player/videoplayer.py
@@ -12,8 +12,8 @@ from pytgcalls.types.input_stream import VideoParameters
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from config import API_ID, API_HASH, SESSION_NAME, BOT_USERNAME
-from helpers.decorators import authorized_users_only
-from helpers.filters import command
+from helper.decorators import authorized_users_only
+from helper.filters import command
 from youtube_dl import YoutubeDL
 from youtube_dl.utils import ExtractorError
 
